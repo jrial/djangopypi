69cfd6cdc6b4805f09927970a5ba9fdad8060c25
Juan Rial <jrial@safehex.be>
2016-11-02 17:41:13 +0100

    Update for Django 1.8: replace django.utils.hashcompat with Python's hashlib

--- djangopypi/signals.py
+++ djangopypi/signals.py
@@ -1,22 +1,22 @@
 import logging
 
 from django.db.models import signals
-from django.utils.hashcompat import md5_constructor
+from hashlib import md5
 
 from djangopypi.models import Package, Release, Distribution
 
 log = logging.getLogger('djangopypi.signals')
 
 def autohide_new_release_handler(sender, instance, created, *args, **kwargs):
-    """ Autohide other releases on the creation of a new release when the 
+    """ Autohide other releases on the creation of a new release when the
     package 'auto-hide' is True"""
     if not created or not instance.package.auto_hide:
         return
-    
+
     for release in instance.package.releases.exclude(pk=instance.pk).filter(hidden=False):
         release.hidden = True
         release.save()
-    
+
     if instance.hidden:
         instance.hidden = False
         instance.save()
@@ -25,28 +25,28 @@ def autohide_save_release_handler(sender, instance, *args, **kwargs):
     """ When saving a release, check to see if it should be hidden or not """
     if instance.pk is None:
         return
-    
+
     if not instance.package.auto_hide:
         return
-    
+
     try:
         latest = instance.package.releases.latest('created')
     except Release.DoesNotExist:
         return
-    
+
     if instance != latest and not instance.hidden:
         instance.hidden = True
 
 def autohide_save_package_handler(sender, instance, *args, **kwargs):
     if not instance.auto_hide:
         return
-    
+
     for release in instance.releases.filter(hidden=False):
         release.save()
 
 def distribution_hash(sender, instance, *args, **kwargs):
     if not instance.md5_digest and instance.content:
-        digest = md5_constructor()
+        digest = md5()
         try:
             fh = instance.content.storage.open(instance.content.name)
             map(digest.update,fh.readlines())
