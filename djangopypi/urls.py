jrial/djangopypi

commit 151783e4300169140b97f5769d45c812e33b9408

Update for Django 1.8: function based views to class based views WIP
FIXMEs marked in urls.py - all sharing same root cause: keyword arg 'pk'

djangopypi/urls.py
@@ -9,25 +9,29 @@ urlpatterns = patterns("djangopypi.views",
     url(r'^search/$','packages.search',name='djangopypi-search'),
     url(r'^pypi/$', 'root', name='djangopypi-release-index'),
     url(r'^rss/$', ReleaseFeed(), name='djangopypi-rss'),
-    
+
+    # FIXME: Keyword arg 'pk'
     url(r'^simple/(?P<package>[\w\d_\.\-]+)/$','packages.simple_details',
         name='djangopypi-package-simple'),
-    
+    # FIXME: Keyword arg 'pk'
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/$','packages.details',
         name='djangopypi-package'),
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/rss/$', ReleaseFeed(),
-        name='djangopypi-package-rss'),    
+        name='djangopypi-package-rss'),
+    # FIXME: Keyword arg 'pk'
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/doap.rdf$','packages.doap',
         name='djangopypi-package-doap'),
+    # FIXME: Keyword arg 'pk'
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/manage/$','packages.manage',
         name='djangopypi-package-manage'),
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/manage/versions/$','packages.manage_versions',
         name='djangopypi-package-manage-versions'),
-    
+
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/(?P<version>[\w\d_\.\-]+)/$',
         'releases.details',name='djangopypi-release'),
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/(?P<version>[\w\d_\.\-]+)/doap.rdf$',
         'releases.doap',name='djangopypi-release-doap'),
+    # FIXME: Keyword arg 'pk'
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/(?P<version>[\w\d_\.\-]+)/manage/$',
         'releases.manage',name='djangopypi-release-manage'),
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/(?P<version>[\w\d_\.\-]+)/metadata/$',

djangopypi/views/packages.py
@@ -4,7 +4,10 @@ from django.http import Http404, HttpResponseRedirect
 from django.forms.models import inlineformset_factory
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
-from django.views.generic import list_detail, create_update
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
+from django.views.generic.base import RedirectView
 
 from djangopypi.decorators import user_owns_package, user_maintains_package
 from djangopypi.models import Package, Release
@@ -13,22 +16,23 @@ from djangopypi.forms import SimplePackageSearchForm, PackageForm
 
 
 def index(request, **kwargs):
-    kwargs.setdefault('template_object_name', 'package')
+    kwargs.setdefault('context_object_name', 'package_list')
     kwargs.setdefault('queryset', Package.objects.all())
-    return list_detail.object_list(request, **kwargs)
+    return ListView.as_view(**kwargs)(request)
 
 def simple_index(request, **kwargs):
     kwargs.setdefault('template_name', 'djangopypi/package_list_simple.html')
     return index(request, **kwargs)
 
 def details(request, package, proxy_folder='pypi', **kwargs):
-    kwargs.setdefault('template_object_name', 'package')
+    kwargs.setdefault('context_object_name', 'package')
     kwargs.setdefault('queryset', Package.objects.all())
+    kwargs.setdefault('pk', package)
     try:
-        return list_detail.object_detail(request, object_id=package, **kwargs)
+        return DetailView.as_view(**kwargs)(request)
     except Http404, e:
         if settings.DJANGOPYPI_PROXY_MISSING:
-            return HttpResponseRedirect('%s/%s/%s/' % 
+            return HttpResponseRedirect('%s/%s/%s/' %
                                         (settings.DJANGOPYPI_PROXY_BASE_URL.rstrip('/'),
                                          proxy_folder,
                                          package))
@@ -42,7 +46,7 @@ def simple_details(request, package, **kwargs):
 
 def doap(request, package, **kwargs):
     kwargs.setdefault('template_name', 'djangopypi/package_doap.xml')
-    kwargs.setdefault('mimetype', 'text/xml')
+    # kwargs.setdefault('mimetype', 'text/xml')
     return details(request, package, **kwargs)
 
 def search(request, **kwargs):
@@ -50,21 +54,21 @@ def search(request, **kwargs):
         form = SimplePackageSearchForm(request.POST)
     else:
         form = SimplePackageSearchForm(request.GET)
-    
+
     if form.is_valid():
         q = form.cleaned_data['query']
-        kwargs['queryset'] = Package.objects.filter(Q(name__contains=q) | 
+        kwargs['queryset'] = Package.objects.filter(Q(name__contains=q) |
                                                     Q(releases__package_info__contains=q)).distinct()
     return index(request, **kwargs)
 
 @user_owns_package()
 def manage(request, package, **kwargs):
-    kwargs['object_id'] = package
+    kwargs['pk'] = package
     kwargs.setdefault('form_class', PackageForm)
     kwargs.setdefault('template_name', 'djangopypi/package_manage.html')
-    kwargs.setdefault('template_object_name', 'package')
+    kwargs.setdefault('context_object_name', 'package')
 
-    return create_update.update_object(request, **kwargs)
+    return UpdateView.as_view(**kwargs)(request)
 
 @user_maintains_package()
 def manage_versions(request, package, **kwargs):
@@ -77,7 +81,7 @@ def manage_versions(request, package, **kwargs):
     kwargs.setdefault('template_name', 'djangopypi/package_manage_versions.html')
     kwargs.setdefault('template_object_name', 'package')
     kwargs.setdefault('extra_context',{})
-    kwargs.setdefault('mimetype',settings.DEFAULT_CONTENT_TYPE)
+    # kwargs.setdefault('mimetype',settings.DEFAULT_CONTENT_TYPE)
     kwargs['extra_context'][kwargs['template_object_name']] = package
     kwargs.setdefault('formset_kwargs',{})
     kwargs['formset_kwargs']['instance'] = package
@@ -86,7 +90,7 @@ def manage_versions(request, package, **kwargs):
         formset = kwargs['formset_factory'](data=request.POST, **kwargs['formset_kwargs'])
         if formset.is_valid():
             formset.save()
-            return create_update.redirect(kwargs.get('post_save_redirect', None),
+            return RedirectView.as_view(kwargs.get('post_save_redirect', None),
                                           package)
 
     formset = kwargs['formset_factory'](**kwargs['formset_kwargs'])
@@ -94,5 +98,5 @@ def manage_versions(request, package, **kwargs):
     kwargs['extra_context']['formset'] = formset
 
     return render_to_response(kwargs['template_name'], kwargs['extra_context'],
-                              context_instance=RequestContext(request),
-                              mimetype=kwargs['mimetype'])
+                              context_instance=RequestContext(request),)
+                            #   mimetype=kwargs['mimetype'])

djangopypi/views/releases.py
@@ -2,38 +2,39 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.forms.models import inlineformset_factory
 from django.http import Http404, HttpResponseRedirect
-from django.views.generic import list_detail, create_update
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
-
 from djangopypi.decorators import user_owns_package, user_maintains_package
 from djangopypi.models import Package, Release, Distribution
 from djangopypi.forms import ReleaseForm, DistributionUploadForm
 
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
+from django.views.generic.base import RedirectView
 
 
 def index(request, **kwargs):
-    kwargs.setdefault('template_object_name','release')
+    kwargs.setdefault('context_object_name','release_list')
     kwargs.setdefault('queryset',Release.objects.filter(hidden=False))
-    return list_detail.object_list(request, **kwargs)
+    return ListView.as_view(**kwargs)(request)
 
 def details(request, package, version, **kwargs):
     release = get_object_or_404(Package, name=package).get_release(version)
-    
+
     if not release:
         raise Http404('Version %s does not exist for %s' % (version,
                                                             package,))
-    
+
     kwargs.setdefault('template_object_name','release')
     kwargs.setdefault('template_name','djangopypi/release_detail.html')
     kwargs.setdefault('extra_context',{})
-    kwargs.setdefault('mimetype',settings.DEFAULT_CONTENT_TYPE)
-    
+    # kwargs.setdefault('mimetype',settings.DEFAULT_CONTENT_TYPE)
+
     kwargs['extra_context'][kwargs['template_object_name']] = release
-        
+
     return render_to_response(kwargs['template_name'], kwargs['extra_context'],
-                              context_instance=RequestContext(request),
-                              mimetype=kwargs['mimetype'])
+                              context_instance=RequestContext(request),)
+                            #   mimetype=kwargs['mimetype'])
 
 def doap(request, package, version, **kwargs):
     kwargs.setdefault('template_name','djangopypi/release_doap.xml')
@@ -43,93 +44,93 @@ def doap(request, package, version, **kwargs):
 @user_maintains_package()
 def manage(request, package, version, **kwargs):
     release = get_object_or_404(Package, name=package).get_release(version)
-    
+
     if not release:
         raise Http404('Version %s does not exist for %s' % (version,
                                                             package,))
-    
-    kwargs['object_id'] = release.pk
-    
+
+    kwargs['pk'] = release.pk
+
     kwargs.setdefault('form_class', ReleaseForm)
     kwargs.setdefault('template_name', 'djangopypi/release_manage.html')
-    kwargs.setdefault('template_object_name', 'release')
-    
-    return create_update.update_object(request, **kwargs)
+    kwargs.setdefault('context_object_name', 'release')
+
+    return UpdateView.as_view(**kwargs)(request)
 
 @user_maintains_package()
 def manage_metadata(request, package, version, **kwargs):
     kwargs.setdefault('template_name', 'djangopypi/release_manage.html')
     kwargs.setdefault('template_object_name', 'release')
     kwargs.setdefault('extra_context',{})
-    kwargs.setdefault('mimetype',settings.DEFAULT_CONTENT_TYPE)
-    
+    # kwargs.setdefault('mimetype',settings.DEFAULT_CONTENT_TYPE)
+
     release = get_object_or_404(Package, name=package).get_release(version)
-    
+
     if not release:
         raise Http404('Version %s does not exist for %s' % (version,
                                                             package,))
-    
+
     if not release.metadata_version in settings.DJANGOPYPI_METADATA_FORMS:
         #TODO: Need to change this to a more meaningful error
         raise Http404()
-    
+
     kwargs['extra_context'][kwargs['template_object_name']] = release
-    
+
     form_class = settings.DJANGOPYPI_METADATA_FORMS.get(release.metadata_version)
-    
+
     initial = {}
     multivalue = ('classifier',)
-    
+
     for key, values in release.package_info.iterlists():
         if key in multivalue:
             initial[key] = values
         else:
             initial[key] = '\n'.join(values)
-    
+
     if request.method == 'POST':
         form = form_class(data=request.POST, initial=initial)
-        
+
         if form.is_valid():
             for key, value in form.cleaned_data.iteritems():
                 if isinstance(value, basestring):
                     release.package_info[key] = value
                 elif hasattr(value, '__iter__'):
                     release.package_info.setlist(key, list(value))
-            
+
             release.save()
-            return create_update.redirect(kwargs.get('post_save_redirect',None),
+            return RedirectView.as_view(kwargs.get('post_save_redirect',None),
                                           release)
     else:
         form = form_class(initial=initial)
-    
+
     kwargs['extra_context']['form'] = form
-    
+
     return render_to_response(kwargs['template_name'], kwargs['extra_context'],
-                              context_instance=RequestContext(request),
-                              mimetype=kwargs['mimetype'])
+                              context_instance=RequestContext(request),)
+                            #   mimetype=kwargs['mimetype'])
 
 @user_maintains_package()
 def manage_files(request, package, version, **kwargs):
     release = get_object_or_404(Package, name=package).get_release(version)
-    
+
     if not release:
         raise Http404('Version %s does not exist for %s' % (version,
                                                             package,))
-    
+
     kwargs.setdefault('formset_factory_kwargs',{})
     kwargs['formset_factory_kwargs'].setdefault('fields', ('comment',))
     kwargs['formset_factory_kwargs']['extra'] = 0
-    
+
     kwargs.setdefault('formset_factory', inlineformset_factory(Release, Distribution, **kwargs['formset_factory_kwargs']))
     kwargs.setdefault('template_name', 'djangopypi/release_manage_files.html')
     kwargs.setdefault('template_object_name', 'release')
     kwargs.setdefault('extra_context',{})
-    kwargs.setdefault('mimetype',settings.DEFAULT_CONTENT_TYPE)
+    # kwargs.setdefault('mimetype',settings.DEFAULT_CONTENT_TYPE)
     kwargs['extra_context'][kwargs['template_object_name']] = release
     kwargs.setdefault('formset_kwargs',{})
     kwargs['formset_kwargs']['instance'] = release
     kwargs.setdefault('upload_form_factory', DistributionUploadForm)
-    
+
     if request.method == 'POST':
         formset = kwargs['formset_factory'](data=request.POST,
                                             files=request.FILES,
@@ -139,23 +140,23 @@ def manage_files(request, package, version, **kwargs):
             formset = kwargs['formset_factory'](**kwargs['formset_kwargs'])
     else:
         formset = kwargs['formset_factory'](**kwargs['formset_kwargs'])
-    
+
     kwargs['extra_context']['formset'] = formset
     kwargs['extra_context'].setdefault('upload_form',
                                        kwargs['upload_form_factory']())
-    
+
     return render_to_response(kwargs['template_name'], kwargs['extra_context'],
-                              context_instance=RequestContext(request),
-                              mimetype=kwargs['mimetype'])
+                              context_instance=RequestContext(request),)
+                            #   mimetype=kwargs['mimetype'])
 
 @user_maintains_package()
 def upload_file(request, package, version, **kwargs):
     release = get_object_or_404(Package, name=package).get_release(version)
-    
+
     if not release:
         raise Http404('Version %s does not exist for %s' % (version,
                                                             package,))
-    
+
     kwargs.setdefault('form_factory', DistributionUploadForm)
     kwargs.setdefault('post_save_redirect', reverse('djangopypi-release-manage-files',
                                                     kwargs={'package': package,
@@ -163,9 +164,9 @@ def upload_file(request, package, version, **kwargs):
     kwargs.setdefault('template_name', 'djangopypi/release_upload_file.html')
     kwargs.setdefault('template_object_name', 'release')
     kwargs.setdefault('extra_context',{})
-    kwargs.setdefault('mimetype',settings.DEFAULT_CONTENT_TYPE)
+    # kwargs.setdefault('mimetype',settings.DEFAULT_CONTENT_TYPE)
     kwargs['extra_context'][kwargs['template_object_name']] = release
-    
+
     if request.method == 'POST':
         form = kwargs['form_factory'](data=request.POST, files=request.FILES)
         if form.is_valid():
@@ -173,14 +174,14 @@ def upload_file(request, package, version, **kwargs):
             dist.release = release
             dist.uploader = request.user
             dist.save()
-            
-            return create_update.redirect(kwargs.get('post_save_redirect'),
+
+            return RedirectView.as_view(kwargs.get('post_save_redirect'),
                                           release)
     else:
         form = kwargs['form_factory']()
-    
+
     kwargs['extra_context']['form'] = form
-    
+
     return render_to_response(kwargs['template_name'], kwargs['extra_context'],
-                              context_instance=RequestContext(request),
-                              mimetype=kwargs['mimetype'])
+                              context_instance=RequestContext(request),)
+                            #   mimetype=kwargs['mimetype'])

commit 43f5c3c152d52824815edddaeef93086b7b919fb

Update for Django 1.8: fix views
Fixed all views left broken in 151783e

djangopypi/urls.py
@@ -10,18 +10,14 @@ urlpatterns = patterns("djangopypi.views",
     url(r'^pypi/$', 'root', name='djangopypi-release-index'),
     url(r'^rss/$', ReleaseFeed(), name='djangopypi-rss'),
 
-    # FIXME: Keyword arg 'pk'
     url(r'^simple/(?P<package>[\w\d_\.\-]+)/$','packages.simple_details',
         name='djangopypi-package-simple'),
-    # FIXME: Keyword arg 'pk'
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/$','packages.details',
         name='djangopypi-package'),
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/rss/$', ReleaseFeed(),
         name='djangopypi-package-rss'),
-    # FIXME: Keyword arg 'pk'
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/doap.rdf$','packages.doap',
         name='djangopypi-package-doap'),
-    # FIXME: Keyword arg 'pk'
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/manage/$','packages.manage',
         name='djangopypi-package-manage'),
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/manage/versions/$','packages.manage_versions',
@@ -31,7 +27,6 @@ urlpatterns = patterns("djangopypi.views",
         'releases.details',name='djangopypi-release'),
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/(?P<version>[\w\d_\.\-]+)/doap.rdf$',
         'releases.doap',name='djangopypi-release-doap'),
-    # FIXME: Keyword arg 'pk'
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/(?P<version>[\w\d_\.\-]+)/manage/$',
         'releases.manage',name='djangopypi-release-manage'),
     url(r'^pypi/(?P<package>[\w\d_\.\-]+)/(?P<version>[\w\d_\.\-]+)/metadata/$',

djangopypi/views/packages.py
@@ -27,9 +27,9 @@ def simple_index(request, **kwargs):
 def details(request, package, proxy_folder='pypi', **kwargs):
     kwargs.setdefault('context_object_name', 'package')
     kwargs.setdefault('queryset', Package.objects.all())
-    kwargs.setdefault('pk', package)
+    # kwargs.setdefault('pk', package)
     try:
-        return DetailView.as_view(**kwargs)(request)
+        return DetailView.as_view(**kwargs)(request, pk=package)
     except Http404, e:
         if settings.DJANGOPYPI_PROXY_MISSING:
             return HttpResponseRedirect('%s/%s/%s/' %
@@ -63,12 +63,13 @@ def search(request, **kwargs):
 
 @user_owns_package()
 def manage(request, package, **kwargs):
-    kwargs['pk'] = package
+    # kwargs['pk'] = package
     kwargs.setdefault('form_class', PackageForm)
+    kwargs.setdefault('queryset', Package.objects.all())
     kwargs.setdefault('template_name', 'djangopypi/package_manage.html')
     kwargs.setdefault('context_object_name', 'package')
 
-    return UpdateView.as_view(**kwargs)(request)
+    return UpdateView.as_view(**kwargs)(request, pk=package)
 
 @user_maintains_package()
 def manage_versions(request, package, **kwargs):

djangopypi/views/releases.py
@@ -49,13 +49,14 @@ def manage(request, package, version, **kwargs):
         raise Http404('Version %s does not exist for %s' % (version,
                                                             package,))
 
-    kwargs['pk'] = release.pk
+    # kwargs['pk'] = release.pk
 
     kwargs.setdefault('form_class', ReleaseForm)
     kwargs.setdefault('template_name', 'djangopypi/release_manage.html')
     kwargs.setdefault('context_object_name', 'release')
+    kwargs.setdefault('queryset', Release.objects.all())
 
-    return UpdateView.as_view(**kwargs)(request)
+    return UpdateView.as_view(**kwargs)(request, pk=release.pk)
 
 @user_maintains_package()
 def manage_metadata(request, package, version, **kwargs):
@@ -175,7 +176,7 @@ def upload_file(request, package, version, **kwargs):
             dist.uploader = request.user
             dist.save()
 
-            return RedirectView.as_view(kwargs.get('post_save_redirect'),
+            return RedirectView.as_view(**kwargs)(kwargs.get('post_save_redirect'),
                                           release)
     else:
         form = kwargs['form_factory']()